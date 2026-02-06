8b19d585259e226c49141eaea52e808eac5f8791
Corey-Code <37006206+Corey-Code@users.noreply.github.com>
2026-02-06 14:51:20 -0500

    refactor TestGenesis to improve variable naming for clarity

--- x/tradebin/module/genesis_test.go
+++ x/tradebin/module/genesis_test.go
@@ -22,7 +22,8 @@ func TestGenesis(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	acc := testutil.NewMockAccountKeeper(ctrl)
 
-	k, ctx := keepertest.TradebinKeeper(t, nil, acc, nil)
+	kVal, ctx := keepertest.TradebinKeeper(t, nil, acc, nil)
+	k := &kVal
 	tradebin.InitGenesis(ctx, k, genesisState)
 	got := tradebin.ExportGenesis(ctx, k)
 	require.NotNil(t, got)
